69104c8cb1080513da05c994ef2edd83b4f43c18
solarizedalias <60473183+solarizedalias@users.noreply.github.com>
2024-08-07 12:53:24 +0900

    work(nvim): custom lsp

--- dot_config/nvim/custom/lua/custom/nvim-lspconfig/init.lua
+++ dot_config/nvim/custom/lua/custom/nvim-lspconfig/init.lua
@@ -362,30 +362,38 @@ else
 
   lspconfig.lua_ls.setup({
     on_init = function(client)
-      local path = client.workspace_folders[1].name
-      if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
-        return
+      -- local path = client.workspace_folders[1].name
+      local path = vim.lsp.buf.list_workspace_folders()[1]
+
+      if path then
+        if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
+          return
+        end
       end
 
-      client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-        runtime = {
-          -- Tell the language server which version of Lua you're using
-          -- (most likely LuaJIT in the case of Neovim)
-          version = 'LuaJIT',
-        },
-        -- Make the server aware of Neovim runtime files
-        workspace = {
-          checkThirdParty = false,
-          library = {
-            libraries,
-            -- Depending on the usage, you might want to add additional paths here.
-            -- "${3rd}/luv/library"
-            -- "${3rd}/busted/library",
+      -- XXX: ???
+      if false then
+        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+          runtime = {
+            -- Tell the language server which version of Lua you're using
+            -- (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT',
           },
-          -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-          -- library = vim.api.nvim_get_runtime_file("", true)
-        },
-      })
+          -- Make the server aware of Neovim runtime files
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              libraries,
+              -- Depending on the usage, you might want to add additional paths here.
+              -- "${3rd}/luv/library"
+              -- "${3rd}/busted/library",
+            },
+            -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+            -- library = vim.api.nvim_get_runtime_file("", true)
+          },
+        })
+      end
+
     end,
     on_attach = custom_attach,
     capabilities = capabilities,
